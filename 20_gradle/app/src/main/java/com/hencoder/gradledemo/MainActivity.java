35ffa2f95289dca7d01dc7c2a3e285f568480699
Kai Zhu <rengwuxian@gmail.com>
2018-12-02 23:00:57 +0800

    20 Gradle 配置文件拆解

---
+++ 20_gradle/app/src/main/java/com/hencoder/gradledemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hencoder.gradledemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BuildTypeUtils.drawBadge(this);
+    }
+}
---
+++ 20_gradle/app/src/release/java/com/hencoder/gradledemo/BuildTypeUtils.java
@@ -0,0 +1,12 @@
+package com.hencoder.gradledemo;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BuildTypeUtils {
+
+    public static void drawBadge(Activity activity) {
+    }
+}
