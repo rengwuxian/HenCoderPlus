5397f6f8625cfc9c01905db641f2f384423b2b74
Kai Zhu <rengwuxian@gmail.com>
2018-12-07 15:11:16 +0800

    Fix: 14

--- 14_view_pager/src/main/java/com/hencoder/a14_view_pager/MainActivity.java
+++ 14_view_pager/src/main/java/com/hencoder/a14_view_pager/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.drag_listener_grid_view);
+        setContentView(R.layout.two_pager);
 
     }
 }
